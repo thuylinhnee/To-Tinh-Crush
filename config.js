3660fa71d56684c451869bea4a51b84b1be32480
thuylinhnee <87000311+thuylinhnee@users.noreply.github.com>
2021-07-06 12:38:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Chào cậu:))',
+    introDesc: `Ngày đó, ngày đó sẽ không xa xôi
+Và chúng ta là người chiến thắng.
+Và ta biết dẫu lắm thác ghềnh cheo leo trên đường xa
+Vượt gian nan ta vươn tới những đỉnh cao.
+Ngày đó, ngày đó sẽ không xa xôi
+Và chúng ta là người chiến thắng
+Đường đến những ngày vinh quang không còn xa
+Dù khó khăn vẫn còn.`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'CHÚC CẬU THI TỐT <3',
+    question:'Bạn của tôi ơi, kỳ thi quan trọng tới rồi, hãy bình tĩnh, thật tự tin để hoàn thành bài thi tốt nhé.',
+    btnReply: 'Gửi cho cậu <3',
+    messDesc: 'Mã đáo thành công nhó :3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/hoangg.thuylinhh.7/'
